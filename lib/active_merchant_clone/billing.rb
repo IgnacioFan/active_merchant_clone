0e69ef5a2db32b32df209703ae963f5d05386e48
nacho <fan01856472@gmail.com>
2022-01-30 22:33:22 +0800

    sort require

--- lib/active_merchant_clone/billing.rb
+++ lib/active_merchant_clone/billing.rb
@@ -1,4 +1,5 @@
 require "active_merchant_clone/billing/credit_card"
-require "active_merchant_clone/billing/credit_card_methods"
-require "active_merchant_clone/billing/credit_card_formatting"
 require "active_merchant_clone/billing/credit_card_brands"
+require "active_merchant_clone/billing/credit_card_expiry_date"
+require "active_merchant_clone/billing/credit_card_formatting"
+require "active_merchant_clone/billing/credit_card_methods"
