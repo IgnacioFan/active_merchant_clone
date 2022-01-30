a410b6838c4bf48cd2955a50fa6f4c9dfc959efc
nacho <fan01856472@gmail.com>
2022-01-30 14:32:32 +0800

    add credit card method test

--- lib/active_merchant_clone/billing/credit_card_methods.rb
+++ lib/active_merchant_clone/billing/credit_card_methods.rb
@@ -1,47 +1,22 @@
+# purpose: provide credit_card object a few methods to validate attributes
+#
 module ActiveMerchantClone
   module Billing
-    # TODO(weilong): rename CreditCardVerifiable
     module CreditCardMethods
       def self.included(base)
-        base.extend(ClassMethods)
+        base.extend(MaskDigits)
       end
 
-      def valid_month?(month)
-        (1..12).cover?(month.to_i)
-      end
+      module MaskDigits
+        def mask(number)
+          "XXXX-XXXX-XXXX-#{last_digits(number)}"
+        end
 
-      def valid_expiry_year?(year)
-        (Time.now.year..(Time.now.year + 20)).cover?(year.to_i)
-      end
-
-      # TODO(weilong): add test mode and add number algorithms
-      def valid_number?(number)
-        valid_card_number_length?(number) &&
-        valid_card_number_characters?(number)
-      end
-
-      module ClassMethods
         def last_digits(number)
           return "" if number.nil?
           number.length <= 4 ? number : number.slice(-4..-1)
         end
-
-        def mask(number)
-          "XXXX-XXXX-XXXX-#{last_digits(number)}"
-        end
       end
-
-      private
-
-        def valid_card_number_length?(number)
-          return false if number.nil?
-          number.length >= 12
-        end
-
-        def valid_card_number_characters?(number)
-          return false if number.nil?
-          !number.match(/\D/)
-        end
     end
   end
 end
